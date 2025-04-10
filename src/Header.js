74e8dc4dbea34fa1c68eff015fdbec346101680e
Akash <akash@katsamsoft.com>
2025-04-10 14:15:57 +0530

    update

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, Container, Button, NavDropdown } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.png'
 import float from './floatimage.png';
@@ -18,6 +18,25 @@ import icon8 from './headericon/8.png'
 import { NavLink } from 'react-router-dom';
 
 export default function Header() {
+
+  const location = useLocation();
+  
+  // Define all service paths
+  const servicePaths = [
+    '/financial-planning',
+    '/portfolio-management-services',
+    '/alternative-investment-funds',
+    '/mutual-fund',
+    '/general-insurance',
+    '/loan',
+    '/will-estate-planning',
+    '/life-insurnace'
+  ];
+
+  // Check if current path starts with any of the service paths
+  const isServiceActive = servicePaths.some(path => location.pathname.startsWith(path));
+
+
   return (
     <>
     
@@ -47,13 +66,16 @@ export default function Header() {
 About Us</NavLink>
 
           <li className="nav-item dropdown custom-dropdown">
-  <NavLink to="#" className={({ isActive }) => isActive ? 'nav-link dropdown-toggle active' : 'nav-link dropdown-toggle'}>
-    Services 
-    &nbsp;<svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5.46967 6.53033C5.76256 6.82322 6.23744 6.82322 6.53033 6.53033L11.3033 1.75736C11.5962 1.46447 11.5962 0.989593 11.3033 0.696699C11.0104 0.403806 10.5355 0.403806 10.2426 0.696699L6 4.93934L1.75736 0.696699C1.46447 0.403806 0.989593 0.403806 0.696699 0.696699C0.403806 0.989592 0.403806 1.46447 0.696699 1.75736L5.46967 6.53033ZM5.25 5L5.25 6L6.75 6L6.75 5L5.25 5Z" fill="#2D2D2D"/>
-</svg>
-
-  </NavLink>
+          <NavLink
+        to="/service"
+        className='nav-link dropdown-toggle'
+      >
+        Services
+        &nbsp;
+        <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.46967 6.53033C5.76256 6.82322 6.23744 6.82322 6.53033 6.53033L11.3033 1.75736C11.5962 1.46447 11.5962 0.989593 11.3033 0.696699C11.0104 0.403806 10.5355 0.403806 10.2426 0.696699L6 4.93934L1.75736 0.696699C1.46447 0.403806 0.989593 0.403806 0.696699 0.696699C0.403806 0.989592 0.403806 1.46447 0.696699 1.75736L5.46967 6.53033ZM5.25 5L5.25 6L6.75 6L6.75 5L5.25 5Z" fill="#2D2D2D"/>
+        </svg>
+      </NavLink>
   <div className="megamenu dropdown-menu">
     <div className="row">
       <div className='col-md-6'>
